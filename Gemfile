7ec08662638be8bbf3705e9712039d7c27a53cd6
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-05-06 13:33:16 -0300

    feat: Adiciona suporte a traduções da data (i18n)

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby "3.0.7"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
 
+
+# Repository for collecting Locale data for Ruby on Rails I18n as well as other interesting, Rails related I18n stuff
+gem 'rails-i18n'
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
 
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,18 @@ class Contact < ApplicationRecord
 belongs_to :kind#, optional: true
 
 
+# def birthdate_br
+  #   I18n.l(self.birthdate) unless self.birthdate.blank?
+  # end
+
+  def to_br
+    {
+      name: self.name,
+      email: self.email,
+      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    }
+  end
+
 # def author
 #     "Vitor Hugo"
 #   end
@@ -17,12 +29,12 @@ belongs_to :kind#, optional: true
 #         )
 #   end
 
-def hello
-    I18n.t('hello')
-  end
+# def hello
+#     I18n.t('hello')
+#   end
 
-  def i18n
-    I18n.default_locale
-  end
+#   def i18n
+#     I18n.default_locale
+#   end   
 
 end
